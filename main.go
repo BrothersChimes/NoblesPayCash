6272e505e6e8294c279e5824b7652c24812b6f5c
Mark Chimes <chimes.mark@gmail.com>
2019-03-16 17:55:54 +0200

    Two Items
    A sword and a bag of trail mix.

--- main.go
+++ main.go
@@ -7,22 +7,55 @@ import (
 	"unicode"
 )
 
-func main() {
-	says("Ulric", "Greetings!")
-	says("Ulric", "I would like to purchase a sword!")
+const uniqueCustName = "Ulric"
 
+func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	var numSwords = 1
+	var numTrailMix = 1
+	announceItemQty("swords", numSwords)
+	announceItemQty("bags of trail mix", numTrailMix)
+
+	says(uniqueCustName, "Greetings!")
+	says(uniqueCustName, "I would like to purchase a sword!")
+
+	sellSwords(reader, &numSwords)
+
+	says(uniqueCustName, "I would still like to purchase a sword!")
+	announceItemQty("swords", numSwords)
+	announceItemQty("bag of trail mix", numSwords)
+
+	sellSwords(reader, &numSwords)
+	announceItemQty("swords", numSwords)
+	announceItemQty("bag of trail mix", numSwords)
+
+}
+
+func sellSwords(reader *bufio.Reader, numSwords *int) {
+	if *numSwords <= 0 {
+		fmt.Println("You inform Ulric that you have no swords left for sale.")
+		return
+	}
+
 	fmt.Println("Would you like to sell Ulric a sword? (y/n)")
-	text, _ := reader.ReadString('\n')
-	var answerChar = unicode.ToUpper(rune(text[0]))
+
+	answer, _ := reader.ReadString('\n')
+	var answerChar = unicode.ToUpper(rune(answer[0]))
+
 	switch answerChar {
 	case 'Y':
-		does("Ulric", "happily takes the sword!")
+		does(uniqueCustName, "happily takes the sword!")
+		*numSwords--
 	case 'N':
-		does("Ulric", "is sad you did not sell him the sword.")
+		does(uniqueCustName, "is sad you did not sell him the sword.")
 	default:
-		does("Ulric", "did not understand what you said.")
+		does(uniqueCustName, "did not understand what you said.")
 	}
+}
+
+func announceItemQty(itemNamePlural string, numSwords int) {
+	fmt.Printf("You have %v %s.\n", numSwords, itemNamePlural)
 }
 
 func does(name, action string) {
