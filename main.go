f198343d5edeaad1daa332ed81636bd584e3cc0e
Mark Chimes <chimes.mark@gmail.com>
2019-03-16 17:24:15 +0200

    Sell a sword
    Player has the option of selling a sword or not.

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt" 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
 
-func main() { 
-	fmt.Printf("Ulric says \"Greetings!\"")
+func main() {
+	says("Ulric", "Greetings!")
+	says("Ulric", "I would like to purchase a sword!")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Would you like to sell Ulric a sword? (y/n)")
+	text, _ := reader.ReadString('\n')
+	var answerChar = unicode.ToUpper(rune(text[0]))
+	switch answerChar {
+	case 'Y':
+		does("Ulric", "happily takes the sword!")
+	case 'N':
+		does("Ulric", "is sad you did not sell him the sword.")
+	default:
+		does("Ulric", "did not understand what you said.")
+	}
+}
+
+func does(name, action string) {
+	fmt.Println(name + " " + action)
+}
+
+func says(name, speech string) {
+	fmt.Println(name + " says: " + "\"" + speech + ".\"")
 }
