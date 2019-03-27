53acda502199bf7965154a80c4394bbf1ccef7f5
Mark Chimes <chimes.mark@gmail.com>
2019-03-27 23:14:24 +0200

    Create new objects for answer provision.

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -13,11 +16,83 @@ type itemType struct {
 	isWeapon   bool
 }
 
+func (i itemType) getAnswer() bool {
+	return i.isWeapon
+}
+
+type reader struct {
+	*bufio.Reader
+}
+
+func (r reader) getAnswer() bool {
+	for true {
+		fmt.Println("Please enter (y/n)")
+		answer, _ := r.ReadString('\n')
+		answerChar := unicode.ToUpper(rune(answer[0]))
+		switch answerChar {
+		case 'Y':
+			return true
+		case 'N':
+			return false
+		default:
+			fmt.Println("That character was not understood.")
+		}
+	}
+	return false
+}
+
+func (r reader) getSelection(maxSelect int) int {
+	for true {
+		fmt.Println("Please enter a number.")
+		answer, _ := r.ReadString('\n')
+		answerNum, err := strconv.Atoi(strings.TrimSpace(answer))
+		if err != nil {
+			log.Fatal(err)
+			fmt.Println("That entry was not understood.")
+			continue
+		}
+
+		if answerNum >= maxSelect {
+			fmt.Println("That number is too high.")
+			continue
+		}
+
+		if answerNum < 0 {
+			fmt.Println("Please enter a non-negative number.")
+			continue
+		}
+
+		return answerNum
+	}
+	return 0
+}
+
+type yesNoAnswerProvider interface {
+	getAnswer() bool
+}
+
+type numericalSelectionProvider interface {
+	getSelection(maxSelect int) int
+}
+
+type answerProvider interface {
+	yesNoAnswerProvider
+	numericalSelectionProvider
+}
+
 const uniqueCustName = "Ulric"
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	reader := reader{bufio.NewReader(os.Stdin)}
+	doSales(reader)
+}
 
+func doSales(reader reader) {
+	itemTypes, storeStock := shopSetup()
+	transactionLoop(reader, itemTypes, &storeStock)
+}
+
+func shopSetup() (map[string]itemType, map[string]int) {
 	itemTypes := map[string]itemType{
 		"sword":    {"sword", "swords", true},
 		"axe":      {"axe", "axes", true},
@@ -30,6 +105,10 @@ func main() {
 		"trailMix": 1,
 	}
 
+	return itemTypes, storeStock
+}
+
+func transactionLoop(reader reader, itemTypes map[string]itemType, storeStock *map[string]int) {
 	says(uniqueCustName, "Hi, Bailoe!")
 
 	customerRequests := []string{"I would like to purchase a weapon!", "I would STILL like to purchase a weapon!", "PLEASE sell me a weapon...", "A weapon, please!"}
@@ -39,18 +118,18 @@ func main() {
 
 	for !isSatisfied {
 		nextString := customerRequests[customerRequestIndex]
-		announceItemQty(storeStock, itemTypes)
+		announceItemQty(*storeStock, itemTypes)
 		says(uniqueCustName, nextString)
 		if customerRequestIndex < len(customerRequests)-1 {
 			customerRequestIndex++
 		}
-		sellWeapons(reader, &storeStock, itemTypes, &isSatisfied)
+		sellWeapons(reader, storeStock, itemTypes, &isSatisfied)
 	}
 	says(uniqueCustName, "Bye, Mr Celhai.")
 	fmt.Println(uniqueCustName + " leaves.")
 }
 
-func sellWeapons(reader *bufio.Reader, stock *map[string]int, types map[string]itemType, isSatisfied *bool) {
+func sellWeapons(reader answerProvider, stock *map[string]int, types map[string]itemType, isSatisfied *bool) {
 	if (*stock)["sword"] <= 0 && (*stock)["axe"] <= 0 {
 		fmt.Println("You inform " + uniqueCustName + " that you have no weapon left for sale.")
 		*isSatisfied = true
@@ -62,23 +141,19 @@ func sellWeapons(reader *bufio.Reader, stock *map[string]int, types map[string]i
 	fmt.Println("[2] Axe")
 	fmt.Println("[0] Nothing")
 
-	answer, _ := reader.ReadString('\n')
-	answerChar := unicode.ToUpper(rune(answer[0]))
-
-	switch answerChar {
-	case '0':
+	selection := reader.getSelection(3)
+	switch selection {
+	case 0:
 		fmt.Println("You decide not to sell anything at this point.")
-	case '1':
+	case 1:
 		sellWeapon(reader, "sword", stock, types)
-	case '2':
+	case 2:
 		sellWeapon(reader, "axe", stock, types)
-	default:
-		fmt.Println("There is no option for \"" + string(answerChar) + "\".")
 	}
 
 }
 
-func sellWeapon(reader *bufio.Reader, weapon string, stock *map[string]int, types map[string]itemType) {
+func sellWeapon(reader yesNoAnswerProvider, weapon string, stock *map[string]int, types map[string]itemType) {
 	weaponName := types[weapon].name
 	weaponPluralName := types[weapon].pluralName
 
@@ -89,17 +164,13 @@ func sellWeapon(reader *bufio.Reader, weapon string, stock *map[string]int, type
 
 	fmt.Println("Would you like to sell Ulric a " + weaponName + "? (y/n)")
 
-	answer, _ := reader.ReadString('\n')
-	answerChar := unicode.ToUpper(rune(answer[0]))
+	answer := reader.getAnswer()
 
-	switch answerChar {
-	case 'Y':
+	if answer {
 		does(uniqueCustName, "happily takes the "+weaponName+".")
 		(*stock)[weapon]--
-	case 'N':
+	} else {
 		does(uniqueCustName, "is sad you did not sell him the "+weaponName+".")
-	default:
-		does(uniqueCustName, "did not understand what you said.")
 	}
 }
 
