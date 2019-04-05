d5c0ea6b117e69c360c0b831b39775749d3ae565
stunbrick <stunbrick@gmail.com>
2019-04-05 23:05:40 +0200

    added shopkeeper object and human interface

--- adventureTracker.go
+++ adventureTracker.go
@@ -2,52 +2,111 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func log(output	string) {
 	fmt.Println(output)
 }
 
-type adventurer struct {
-	home		string
-	location	string
-	gold		int
-	items		[]string
+type Human interface {
+	Gold() int
+	GoHome() string
 }
 
-func (a *adventurer) go_home() string {
+type shopkeeper struct {
+	gold		int
+	items		map[string]bool
+	shop		string
+}
+
+type adventurer struct {
+	gold		int
+	home		string
+	items		[]string
+	location	string
+	weapon		string
+}
+
+
+func (s *shopkeeper) Gold() int{
+	return s.gold
+}
+
+func (s *shopkeeper) GoHome() string {
+	return s.shop
+}
+
+func (s *shopkeeper) AddGold(amt int) int {
+	s.gold += amt
+	return s.gold
+}
+
+func (s *shopkeeper) RemoveItem(itemName string) string {
+	delete(s.items, itemName)
+	return itemName
+}
+
+func (s *shopkeeper) SellItem(itemName string) string {
+	if (itemName == "Blackstaff") {
+		s.AddGold(20)
+	} else if (itemName == "Longstaff") {
+		s.AddGold(10)
+	} else {
+		return "big fat sack of nothing"
+	}
+	return s.RemoveItem(itemName)
+}
+
+func (a *adventurer) SwingWeapon() string{
+	return "hit with his " + a.weapon + " for 3 damage";
+}
+
+func (a *adventurer) GoHome() string {
 	//adventurer goes to his home
 	a.location = a.home
 	return a.location
 }
 
-func (a *adventurer) remove_gold(amt int) int {
+func (a *adventurer) Gold() int {
+	return a.gold
+}
+
+func (a *adventurer) RemoveGold(amt int) int {
 	//adventurer loses amt gold		returns remainder
 	a.gold -= amt
 	return a.gold
 }
 
-func (a *adventurer) add_item(itemName string) string {
+func (a *adventurer) AddItem(itemName string) string {
 	a.items = append(a.items, itemName)
 	return itemName
 }
 
-func (a *adventurer) buy_item(itemName string) string {
+func (a *adventurer) BuyItem(itemName string) string {
 	if (itemName == "sword" && a.gold >= 8) {
-			a.remove_gold(8)
-			return a.add_item("sword")
-	} else {
-		return "big fat sack of nothing"
+		a.RemoveGold(8)
+		return a.AddItem("sword")
 	}
+	return "big fat sack of nothing"
+}
+
+func GoHomeCountMoney(h Human) {
+	log("The Human has arrived in " + h.GoHome());
+	log("The Human has " + strconv.Itoa(h.Gold()) + "GP");
 }
 
 func main() {
-	dirk := adventurer{home: "town", location: "goblin caves", gold: 10}
-	log("Dirk the Adventurer has arrived in " + dirk.go_home());
-	log("Dirk the Adventurer has bought a " + dirk.buy_item("sword"));
+	dirk := &adventurer{home: "town", location: "goblin caves", gold: 10}
+	shillelagh := &shopkeeper{shop: "Shilling Staves", gold: 1000, items: map[string]bool{"Longstaff": true, "Blackstaff": true}}
+	log("Dirk the Adventurer has arrived in " + dirk.GoHome());
+	log("Dirk the Adventurer has " + strconv.Itoa(dirk.Gold()) + "GP");
+	log("Dirk the Adventurer has bought a " + dirk.BuyItem("sword"));
+	log("Dirk the Adventurer has " + strconv.Itoa(dirk.Gold()) + "GP");
 	log("Dirk the Adventurer has left town");
 	log("Dirk the Adventurer has arrived at the mouth of the caves");
 	log("Dirk the Adventurer has encounted 2 goblins");
+	log("Dirk the Adventurer has " + dirk.SwingWeapon());
 	log("Dirk the Adventurer has killed a goblin");
 	log("Dirk the Adventurer has taken 1 damage");
 	log("Dirk the Adventurer has killed a goblin");
@@ -60,5 +119,11 @@ func main() {
 	log("Dirk the Adventurer has sold a sword");
 	log("Dirk the Adventurer has handed in quest 'deal with the goblin raiders'");
 	log("Dirk the Adventurer has received a scroll of healing");
+	log("Shillelagh the Shoppekeep has " + strconv.Itoa(shillelagh.Gold()) + "GP");
+	log("Shillelagh the Shoppekeep has sold " + shillelagh.SellItem("Blackstaff"));
+	log("Shillelagh the Shoppekeep has " + strconv.Itoa(shillelagh.Gold()) + "GP");
+	GoHomeCountMoney(shillelagh)
+	GoHomeCountMoney(dirk)
+
 }
 
