91f1743d4a96c0baf643cba743413927464e7f1b
Mark <chimes.mark@gmail.com>
2019-05-19 13:53:59 +0200

    Use a slice of stock items rather than a map from strings.

--- item/type.go
+++ item/type.go
@@ -1,8 +1,9 @@
 package item
 
-// Type designates information for items that can be
+// Type designates information for items that can be bought and sold
 type Type struct {
 	Name       string
 	PluralName string
+	UseAn      bool
 	IsWeapon   bool
 }
--- shop/shop.go
+++ shop/shop.go
@@ -16,58 +16,66 @@ const uniqueCustName = "Ulric"
 
 // DoSales sets up a stock and then allows the player to sell the items
 func DoSales(provider selection.AnswerProvider) {
-	itemTypes, storeStock := Setup()
-	TransactionLoop(provider, itemTypes, &storeStock)
+	stock := Setup()
+	TransactionLoop(provider, stock)
+}
+
+// StockItem represents an item combined with its stock-relevant information such as quantity and price
+type StockItem struct {
+	*item.Type
+	qty int
 }
 
 // Setup prepares a stock of basic items for trying out sales
-func Setup() (map[string]item.Type, map[string]int) {
-	itemTypes := map[string]item.Type{
-		"sword":    {Name: "sword", PluralName: "swords", IsWeapon: true},
-		"axe":      {Name: "axe", PluralName: "axes", IsWeapon: true},
-		"trailMix": {Name: "bag of trail mix", PluralName: "bags of trail mix", IsWeapon: false},
-	}
+func Setup() []StockItem {
+	stock := make([]StockItem, 3)
 
-	storeStock := map[string]int{
-		"sword":    1,
-		"axe":      1,
-		"trailMix": 1,
-	}
+	stock[0] = StockItem{&item.Type{Name: "sword", PluralName: "swords", UseAn: false, IsWeapon: true}, 1}
+	stock[1] = StockItem{&item.Type{Name: "axe", PluralName: "axes", UseAn: true, IsWeapon: true}, 1}
+	stock[2] = StockItem{&item.Type{Name: "bag of trail mix", UseAn: false, PluralName: "bags of trail mix", IsWeapon: false}, 1}
 
-	return itemTypes, storeStock
+	return stock
 }
 
 // TransactionLoop loops across the customer until they are satisfied
-func TransactionLoop(provider selection.AnswerProvider, itemTypes map[string]item.Type, storeStock *map[string]int) {
+func TransactionLoop(provider selection.AnswerProvider, stock []StockItem) {
 	says(uniqueCustName, "Hi, Bailoe!")
 
 	customerRequests := []string{"I would like to purchase a weapon!", "I would STILL like to purchase a weapon!", "PLEASE sell me a weapon...", "A weapon, please!"}
 	customerRequestIndex := 0
 
-	isSatisfied := false
+	isSoldOut := false
 
-	for !isSatisfied {
+	for !isSoldOut {
 		nextString := customerRequests[customerRequestIndex]
-		announceItemQty(*storeStock, itemTypes)
+		announceItemQty(stock)
 		says(uniqueCustName, nextString)
 		if customerRequestIndex < len(customerRequests)-1 {
 			customerRequestIndex++
 		}
-		SellWeapons(provider, storeStock, itemTypes, &isSatisfied)
+		isSoldOut = SellWeapons(provider, stock)
 	}
-	says(uniqueCustName, "Bye, Mr Celhai.")
+	says(uniqueCustName, "Bye, Celhai.")
 	fmt.Println(uniqueCustName + " leaves.")
 }
 
 // SellWeapons provides a list of available weapons and asks which one should be sold
-func SellWeapons(provider selection.AnswerProvider, stock *map[string]int, types map[string]item.Type, isSatisfied *bool) {
-	if (*stock)["sword"] <= 0 && (*stock)["axe"] <= 0 {
-		fmt.Println("You inform " + uniqueCustName + " that you have no weapon left for sale.")
-		*isSatisfied = true
-		return
+func SellWeapons(provider selection.AnswerProvider, stock []StockItem) (isSoldOut bool) {
+
+	isSoldOut = true
+	for _, item := range stock {
+		if item.IsWeapon && item.qty > 0 {
+			isSoldOut = false
+		}
 	}
 
-	SellWeaponsFoo(provider, stock, types, isSatisfied)
+	if isSoldOut {
+		fmt.Printf("You inform %s that you have no weapons left for sale.\n", uniqueCustName)
+		return isSoldOut
+	}
+
+	SellWeaponsFoo(provider, stock)
+	return isSoldOut
 }
 
 func sortedKeys(stock *map[string]int) []string {
@@ -84,66 +92,65 @@ func sortedKeys(stock *map[string]int) []string {
 }
 
 // SellWeaponsFoo ...
-func SellWeaponsFoo(provider selection.AnswerProvider, stock *map[string]int, types map[string]item.Type, isSatisfied *bool) {
-	fmt.Println("What would you like to sell Ulric?")
+func SellWeaponsFoo(provider selection.AnswerProvider, stock []StockItem) {
+	fmt.Printf("What would you like to sell %s?\n", uniqueCustName)
 
-	map2 := map[string]int{}
-
-	/* Copy Content from Map1 to Map2*/
-	for key, value := range *stock {
-		if value > 0 {
-			map2[key] = value
-		}
-	}
+	stock2 := make([]*StockItem, len(stock)+1)
 
 	i := 1
-	keys := sortedKeys(&map2)
-	for _, key := range keys {
-		fmt.Printf("[%v] %s\n", i, strings.Title(types[key].Name))
-		i++
+	for j, item := range stock {
+		if item.qty > 0 {
+			stock2[i] = &stock[j]
+			fmt.Printf("[%v] %s\n", i, strings.Title(item.Name))
+			i++
+		}
 	}
 	fmt.Println("[0] Nothing")
 
-	selection := provider.GetSelection(len(keys) + 1)
+	selection := provider.GetSelection(len(stock2))
 
 	if selection == 0 {
 		fmt.Println("You decide not to sell anything at this point.")
 		return
 	}
 
-	SellWeapon(provider, keys[selection-1], stock, types)
+	SellWeapon(provider, stock2[selection])
 }
 
 // SellWeapon tries to sell a single weapon
-func SellWeapon(provider selection.YesNoAnswerProvider, weapon string, stock *map[string]int, types map[string]item.Type) {
-	weaponName := types[weapon].Name
-	weaponPluralName := types[weapon].PluralName
+func SellWeapon(provider selection.YesNoAnswerProvider, item *StockItem) {
+	weaponName := item.Name
+	weaponPluralName := item.PluralName
+	aOrAn := "a"
+	if item.UseAn {
+		aOrAn = "an"
+	}
 
-	if (*stock)[weapon] <= 0 {
+	if item.qty <= 0 {
 		fmt.Println("You have no " + weaponPluralName + " left for sale.")
 		return
 	}
 
-	if !types[weapon].IsWeapon {
-		fmt.Printf("Ulric asks how he is meant to kill goblins with a %s? ", weaponName)
+	if !item.IsWeapon {
+		fmt.Printf("%s asks how they are meant to kill goblins with %s %s ?\n", uniqueCustName, aOrAn, weaponName)
 		return
 	}
 
-	fmt.Println("Would you like to sell Ulric a " + weaponName + "? (y/n)")
+	fmt.Printf("Would you like to sell %s %s %s ? (y/n)\n", uniqueCustName, aOrAn, weaponName)
 
 	answer := provider.GetAnswer()
 
 	if answer {
 		does(uniqueCustName, "happily takes the "+weaponName+".")
-		(*stock)[weapon]--
+		item.qty--
 	} else {
-		does(uniqueCustName, "is sad you did not sell him the "+weaponName+".")
+		does(uniqueCustName, "is sad you did not sell them the "+weaponName+".")
 	}
 }
 
-func announceItemQty(stock map[string]int, types map[string]item.Type) {
-	for name, qty := range stock {
-		fmt.Printf("You have %v %s.\n", qty, types[name].PluralName)
+func announceItemQty(stock []StockItem) {
+	for _, item := range stock {
+		fmt.Printf("You have %v %s.\n", item.qty, item.PluralName)
 	}
 }
 
